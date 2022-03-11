03abe1fbc3a3f18cb3fc26d272fbca1ed2c263c4
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-12 01:21:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // YANG SUDAH DONASI
 
 let fs = require('fs')
-global.owner = ['6287833118871', '6287833118871','6287833118871', '6287833118871', '6287833118871'] // Letakan nomor kamu disini
+global.owner = ['6287833118871', '6287833118871','6285828764046', '6285828764046', '6285828764046'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
