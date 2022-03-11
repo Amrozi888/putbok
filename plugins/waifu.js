a4f9fdcb40dd9695d9714a26d9702c8a492310ec
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-12 01:58:40 +0700

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let res = await fetch(API('waifupics', '/sfw/waifu'))
   if (!res.ok) throw eror
   let json = await res.json()
-  conn.sendButtonImg(m.chat, json.url, 'Waifu', '© zifabotz2', 'Waifu', usedPrefix + command, m)
+  conn.sendButtonImg(m.chat, json.url, 'Waifu', '© dikabotz', 'Waifu', usedPrefix + command, m)
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
