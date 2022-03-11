d0cab642e9955573248de1cd4f4a2be1ca730252
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-11 23:56:35 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'donasi', `
-┌「 *Topap • Maxis* 」
-├ 60147366955
+┌「 *Dana • Gopay* 」
+├ 085828764046
 └────
-`.trim(), '© Putbotz', 'Donasi', '.donasi', m)
+`.trim(), '© zifabotz2', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
