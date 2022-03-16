687c6d534879e5b7e6aecd767dd2dfba1ca9effe
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-16 14:43:03 +0700

    Update tahta2.js

--- plugins/tahta2.js
+++ plugins/tahta2.js
@@ -27,14 +27,14 @@ let handler = async (m, { text, usedPrefix, command }) => {
       ])
         .on('error', () => m.reply(`_*Error!*_`))
         .on('exit', () => {
-          conn.sendFile(m.chat, './src/Aesthetic/tahta.jpg', 'harta5.jpg', '© dikabotz', m)
+          conn.sendFile(m.chat, './src/Aesthetic/tahta.jpg', 'harta5.jpg', '© zifabotz', m)
           fs.unlinkSync('./src/Aesthetic/tahta.jpg')
         })
     } catch (e) {
       console.log(e)
       throw eror
     }
-  } else throw `contoh:\n${usedPrefix + command} dikabotz`
+  } else throw `contoh:\n${usedPrefix + command} zifabotz`
 }
 handler.help = ['harta2'].map(v => v + ' <teks>')
 handler.tags = ['tools']
