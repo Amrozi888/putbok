e2e0e377b0df4043d1953eb72612f7e44cc45c00
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-16 14:01:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // YANG SUDAH DONASI
 
 let fs = require('fs')
-global.owner = ['6287833118871', '6287833118871','6285828764046', '6285828764046', '6285828764046'] // Letakan nomor kamu disini
+global.owner = ['6285828764046', '6285828764046','6285828764046', '6285828764046', '6285828764046'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -41,15 +41,15 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'DIKA'
-global.author = '⳹ ❋ཻུ۪۪DIKABOTZ⳹ ❋ཻུ۪۪⸙'
+global.packname = 'rozi'
+global.author = '⳹ ❋ཻུ۪۪ZIFABOTZ⳹ ❋ཻུ۪۪⸙'
 
-global.wait = '「 ⏱️ 」TUNGGU KAK LAGI LOAD...'
+global.wait = '「 ⏱️ 」TUNGGU KAK ZIFABOTZ LAGI PROSES...'
 global.eror = '「❗」Server LAGI ERROR KAK!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '©️ ⳹ ❋ཻུ۪۪DIKABOTZ⳹ ❋ཻུ۪۪⸙' //change the watermark 
+global.watermark = '©️ ⳹ ❋ཻུ۪۪zifabotz⳹ ❋ཻུ۪۪⸙' //change the watermark 
 global.image = 'https://telegra.ph/file/052ca138a613c8859756f.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/577547f8a75d6c38f1412.jpg'
+global.thumbfoto = 'https://telegra.ph/file/442fa7a6bad9b4bfcd9c.jpg'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
